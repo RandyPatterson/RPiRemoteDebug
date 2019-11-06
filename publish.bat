fc562a0ce2933f12b8d4f8f84bdb60fefe6dd55d
Randy Patterson <randy@randypatterson.com>
2019-11-06 14:46:19 -0500

    add some config parameters

--- publish.bat
+++ publish.bat
@@ -1,6 +1,4 @@
-dotnet publish -r linux-arm -o linux-arm /p:ShowLinkerSizeComparison=true 
-rem pushd .\linux-arm
+dotnet publish -r linux-arm -o .\linux-arm /p:ShowLinkerSizeComparison=true 
 plink -pw "raspberry" -batch root@192.168.10.96 mkdir /home/pi/worker
 pscp -pw raspberry -batch -v -r .\linux-arm\* root@192.168.10.96:/home/pi/worker
 plink -pw "raspberry" -batch root@192.168.10.96 chmod 777 /home/pi/worker/rpiworker
-rem popd
